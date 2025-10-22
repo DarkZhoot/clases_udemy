d762a4f6733b4260af29de7af284809cdf7ddbf3
DarkZhoot <71627203+DarkZhoot@users.noreply.github.com>
2025-10-21 23:46:03 -0500

    subiendo mi repo

---
+++ peliculas/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Modulo de peliculas',
+    'version': '1.0',
+    'depends': [
+        'contacts'
+    ],
+    'author': 'Gustavo',
+    'category': 'Peliculas',
+    'website': 'https://www.google.com/?hl=es',
+    'description': '''
+    Modulo para hacer presupuestos de peliculas
+    ''',
+    'summary': 'Modulo de presupuestos para peliculas',
+    'data' : [
+        'views/presupuesto_views.xml',
+        'views/menu.xml',
+    ],
+}
---
+++ peliculas/models/presupuesto.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+class Presupuesto(models.Model):
+    _name = "presupuesto"
+    name = fields.Char(string='Pelicula')
+    clasificacion = fields.Selection(
+        selection=[
+            ("G", "G"), #Publico gemeral
+            ("PG", "PG"), #Se recomienda la compania de un adulto
+            ("PG-13", "PG-13"), #Mayoes de 13
+            ("R", "R"), #Mayores de 18
+        ], string="Clasificacion")
+    fch_estreno = fields.Date(string='Fecha estreno')
+    puntuacion = fields.Integer(string='Puntuacion')
+    active = fields.Boolean(string='Activo', default=True)
+    director_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Director'
+    )
+    genero_ids = fields.Many2many(
+        comodel_name='genero',
+        string='Generos'
+    )
+    vista_general = fields.Text(string='Descripcion')
